010168e2e1d1a931e2edef6601eaaadd3999991f
Tiago <tiagowebersc@gmail.com>
2019-07-12 09:31:35 +0200

    first commit

---
+++ Day 1/oop_introduction.php
@@ -0,0 +1,18 @@
+<?php
+/*
+    Pros of OOP:
+        - Reusability
+        - Roles separation
+
+    Warning, you can't re-declare a class: differents names
+*/
+
+// Declare a class
+class Car
+{ }
+
+// create an object : instance of a class
+$myCar = new Car();
+// we can create as much objects as we want
+$ferrari = new Car();
+$lada = new Car();

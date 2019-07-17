4a0c720c98359d27e173707276c33faf122706ad
Tiago <tiagowebersc@gmail.com>
2019-07-17 10:24:45 +0200

    Abstract class / interface concept

--- Day 3/interface.php
+++ Day 3/interface.php
@@ -32,6 +32,29 @@ class Dog extends Animal implements IDomestic
     // must to be implemented
     public function sit()
     {
-        return "OK!";
+        return "Ok!";
     }
 }
+class Cat extends Animal implements IDomestic, IExample
+{
+    // must to be implemented
+    public function sit()
+    {
+        return "No!";
+    }
+}
+
+/*
+Abstract class:
+1 - When a model/template must be set for a group of subclasses, use an abstract class.
+2 - An abstract class can contain concrete methods
+3 - Abstract classes provide default actions for subclasses
+4 - Used to provide a template for potential future inheritad class.
+
+Interface:
+1 - Use an interface when a role must be defined for ohter classes as well, regardless of the inheritance of these classes.
+2 - You can't instantiate an interface and create an object
+3 - Interface can't contain concrete methods
+4 - An class can implement multiple interfaces
+5 - An interface can inherit from a class
+*/

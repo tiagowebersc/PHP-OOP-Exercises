4f8f11d0d35c624530d031243335b34a4215a9c3
Tiago <tiagowebersc@gmail.com>
2019-07-29 11:05:08 +0200

    Handling json

--- Day 6/Movie.php
+++ Day 6/Movie.php
@@ -10,3 +10,24 @@ class Movie
         $this->releaseYear = $releaseYear;
     }
 }
+
+class MoviePri implements JsonSerializable
+{
+    private $title;
+    private $releaseYear;
+
+    public function __construct($title, $releaseYear)
+    {
+        $this->title = $title;
+        $this->releaseYear = $releaseYear;
+    }
+
+    // Overrride the method from JsonSerializable
+    public function jsonSerialize()
+    {
+        return [
+            "title" => $this->title,
+            "releaseYear" => $this->releaseYear
+        ];
+    }
+}
--- Day 6/json_serialize.php
+++ Day 6/json_serialize.php
@@ -12,7 +12,14 @@ var_dump($myJson);
 
 
 require_once 'Movie.php';
+// Class with public attributes
 $jurrasic = new Movie("Jurassic park", 2018);
+var_dump($jurrasic);
+$json = json_encode($jurrasic, JSON_PRETTY_PRINT);
+var_dump($json);
+
+// Class with private attributes using interface JsonSerializable
+$jurrasic = new MoviePri("Jurassic park", 2018);
 var_dump($jurrasic);
 $json = json_encode($jurrasic, JSON_PRETTY_PRINT);
 var_dump($json);
